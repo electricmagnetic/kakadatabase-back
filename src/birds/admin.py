68d8fa20d63d68f7d990640b101c4d3843beb913
George Moon <georgemoon@users.noreply.github.com>
2019-12-10 16:29:24 +0100

    Added Bird and Area models (#3)

--- src/birds/admin.py
+++ src/birds/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bird
+
+class BirdAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('__str__', 'area',)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.all().select_related('area',)
+
+admin.site.register(Bird, BirdAdmin)
--- src/locations/models.py
+++ src/locations/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Area(models.Model):
+    """ Basic location information """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
