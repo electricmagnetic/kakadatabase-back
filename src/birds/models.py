43633ac2922f55017bd980bf38082e6e3b65835a
George Moon <george.moon@catalyst-eu.net>
2019-12-10 16:28:51 +0100

    Added Bird and Area models

--- src/birds/models.py
+++ src/birds/models.py
@@ -1,3 +1,28 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from locations.models import Area
+
+class Bird(models.Model):
+    """ Basic bird information """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+
+    area = models.ForeignKey(Area,
+        related_name='birds',
+        on_delete=models.PROTECT,
+    )
+
+    primary_band = models.CharField(max_length=20)
+
+    # Metadata
+    date_modified = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
