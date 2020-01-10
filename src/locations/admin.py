91d56b15b22af141901285010b8a17c511d6aad3
George Moon <george@moon.org.nz>
2020-01-10 14:23:47 +0100

    Added Place and Region location objects

--- src/locations/admin.py
+++ src/locations/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Area
+from .models import Area, Region, Place
 
 admin.site.register(Area)
+admin.site.register(Region)
+admin.site.register(Place)
--- src/locations/models.py
+++ src/locations/models.py
@@ -1,6 +1,7 @@
 import uuid
 
-from django.db import models
+from django.contrib.gis.db import models
+from django.utils.text import slugify
 
 class Area(models.Model):
     """ Basic location information """
@@ -13,3 +14,43 @@ class Area(models.Model):
 
     def __str__(self):
         return self.name
+
+class Place(models.Model):
+    """ Place used to generate geocode strings """
+
+    name_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    feat_type = models.CharField(max_length=200, blank=True, null=True)
+    land_district = models.CharField(max_length=200, blank=True, null=True)
+
+    point = models.PointField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Region(models.Model):
+    """ Wider regions, designed to be made up of imported StudyArea objects """
+
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, primary_key=True, editable=False)
+
+    description = models.CharField(max_length=200, blank=True, null=True)
+
+    areas = models.ManyToManyField(Area)
+
+    polygon = models.PolygonField(null=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        """ Generate slug from name """
+
+        self.slug = slugify(self.name)
+        super(Region, self).save(*args, **kwargs)
